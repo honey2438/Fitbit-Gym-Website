571500bef815fd81e154fa305ad2305c42434a18
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-06-26 21:59:06 +0530

    nineth commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const hbs=require('pug');
-const port=process.env.port || 8000;
+const port=process.env.port || 3338;
 const path=require("path");
 
 app.use('/static', express.static('static'));
