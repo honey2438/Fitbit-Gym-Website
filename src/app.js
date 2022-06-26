5613be62af062d76179fae995577cc7e657d1922
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-06-26 19:45:26 +0530

    Third Commit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use('/static', express.static('static'));
 app.use(express.urlencoded());
 
 app.set('view engine','pug');
-app.set('views',path.join(__dirname,'views'));
+app.set('views',path.join(__dirname,'../views'));
 
 app.get("/index",(req,res)=>{
     const param={'title':"Home"};
