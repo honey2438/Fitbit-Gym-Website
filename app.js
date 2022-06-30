2495cca68c5046c43e68b70c5fb299d38b043ddc
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-06-30 12:41:16 +0530

    "twelfth commit"

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express=require("express");
 const app=express();
-const hbs=require('pug');
-const port=process.env.port || 8080;
+const port= process.env.PORT || 8000;
 const path=require("path");
-const http=require("http");
+const fs=require("fs");
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(`mongodb://localhost:27017/newdatabase`);
+};
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded());
 
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
-
 app.get("/index",(req,res)=>{
     const param={'title':"Home"};
     res.status(200).render('index.pug',param);
@@ -19,6 +24,24 @@ app.get("/contact",(req,res)=>{
     const param={'title':"Contact us"};
     res.status(200).render('contact.pug',param);
 });
+const contactSchema=new mongoose.Schema({
+    nam:String,
+    age:String,
+    city:String,
+    mobileNo:String
+});
+const contact=mongoose.model('contact',contactSchema);
+app.post("/contact",(req,res)=>{
+    var myData = new contact(req.body);
+    myData.save().then(()=>{
+    res.send("This item has been saved to the database")
+    }).catch(()=>{
+    res.status(400).send("item was not saved to the databse")
+    }
+    )
+});
+
+
 app.get("/about",(req,res)=>{
     const param={'title':"About us"};
     res.status(200).render('about.pug',param);
