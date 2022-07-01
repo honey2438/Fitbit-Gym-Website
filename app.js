f36e015891cc4f730c68c00e138bf00685d0ddbc
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-07-01 22:18:32 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const pug =require("pug");
 const port = process.env.PORT || 8000;
 const path = require("path");
-const fs = require("fs");
 const mongoose = require("mongoose");
+const bodyParser=require("body-parser");
 
 main().catch((err) => console.log(err));
 
@@ -14,8 +15,9 @@ async function main() {
 app.use("/static", express.static("static"));
 app.use(express.urlencoded());
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
 app.get("/index", (req, res) => {
   const param = { title: "Home" };
   res.status(200).render("index.pug", param);
