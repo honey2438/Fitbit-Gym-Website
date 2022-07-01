729f5587904ca708216ef1fa0e105ef112c0e138
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-07-01 22:57:30 +0530

    commit

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ const app = express();
 const pug =require("pug");
 const port = process.env.PORT || 8000;
 const path = require("path");
-const mongoose = require("mongoose");
 const bodyParser=require("body-parser");
 
 main().catch((err) => console.log(err));
-
-async function main() {
-  await mongoose.connect(`mongodb://localhost:27017/newdatabase`);
-}
 
 app.use("/static", express.static("static"));
 app.use(express.urlencoded());
@@ -26,27 +21,6 @@ app.get("/", (req, res) => {
 app.get("/contact", (req, res) => {
   const param = { title: "Contact us" };
   res.status(200).render("contact.pug", param);
-});
-
-const contactSchema = new mongoose.Schema({
-  nam: String,
-  age: String,
-  city: String,
-  mobileNo: String,
-});
-
-const contact = mongoose.model("contact", contactSchema);
-
-app.post("/contact", (req, res) => {
-  var myData = new contact(req.body);
-  myData
-    .save()
-    .then(() => {
-      res.send("This item has been saved to the database");
-    })
-    .catch(() => {
-      res.status(400).send("item was not saved to the databse");
-    });
 });
 
 app.get("/about", (req, res) => {
