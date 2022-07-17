6fc15ee50375cb6dec488edc76b3b45c87bcb827
Devansh Sirohi <91769500+honey2438@users.noreply.github.com>
2022-07-17 10:29:26 +0530

    commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require("path");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 require("dotenv").config();
-require("static/contact.js");
   
 //mongoose Connection
   mongoose.connect(process.env.MONGOOSE_URL_STRING,{
